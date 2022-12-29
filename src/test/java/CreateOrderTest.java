1ac703ddbcad552cddc00ba2df603ea03d54fc7b
MikkleUp <Jestb74@mail.ru>
2022-12-29 11:42:18 +0500

    add src, test, change Pom

---
+++ src/test/java/CreateOrderTest.java
@@ -0,0 +1,104 @@
+import client.IngredientClient;
+import client.OrderClient;
+import client.UserClient;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import model.Ingredient;
+import model.Order;
+import model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class CreateOrderTest {
+    public User user;
+    public UserClient userClient;
+    public OrderClient orderClient;
+    public Order order;
+    public Ingredient allIngredient;
+    public IngredientClient ingredientClient;
+    private String accessToken;
+    public List<String> ingredients;
+    Response response;
+
+    @Before
+    public void setup() {
+        ingredientClient = new IngredientClient();
+        allIngredient = ingredientClient.getIngredient();
+        ingredients = new ArrayList<>();
+        ingredients.add(allIngredient.data.get(0).get_id());
+        ingredients.add(allIngredient.data.get(1).get_id());
+        ingredients.add(allIngredient.data.get(2).get_id());
+        user = User.getRandomUser();
+        userClient = new UserClient();
+        userClient.create(user);
+        Response responseForToken = userClient.login(user);
+        responseForToken.then().assertThat().body("success", equalTo(true)).and().statusCode(200);
+        accessToken = responseForToken.then().extract().path("accessToken");
+
+    }
+
+    @Test
+    @DisplayName("Creating an order with authorization")
+    public void createOderWithAuthorizationTest() {
+        order = new Order(ingredients);
+        orderClient = new OrderClient();
+        response = orderClient.createOrderWithLogin(accessToken, order);
+        response.then().assertThat().body("success", equalTo(true)).and().statusCode(200);
+
+    }
+
+
+    @Test
+    @DisplayName("Creating an order without authorization")
+    public void createOderWithoutAuthorizationTest() {
+        orderClient = new OrderClient();
+        order = new Order(ingredients);
+        response = orderClient.createOrderWithoutLogin(order);
+        response.then().assertThat().statusCode(200).and().body("success", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Create order with ingredients")
+    public void createOderWithIngredient() {
+
+        order = new Order(ingredients);
+        orderClient = new OrderClient();
+        response = orderClient.createOrderWithLogin(accessToken, order);
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Create order without ingredients")
+    public void createOderWithNullIngredient() {
+
+        ingredients.clear();
+        order = new Order(ingredients);
+        orderClient = new OrderClient();
+        response = orderClient.createOrderWithLogin(accessToken, order);
+        response.then().assertThat().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Create order with wrong hash ingredients")
+    public void createOderWithIncorrectIngredient() {
+
+        ingredients.add("IncorrectIngredient");
+        order = new Order(ingredients);
+        orderClient = new OrderClient();
+        response = orderClient.createOrderWithLogin(accessToken, order);
+        response.then().assertThat().statusCode(500);
+    }
+
+    @After
+    public void deleteUser(){
+        userClient.deleteUser();
+    }
+
+}
---
+++ src/test/java/RegistrationUserTest.java
@@ -0,0 +1,66 @@
+import client.UserClient;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import model.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class RegistrationUserTest {
+    public UserClient userClient;
+    public User user;
+    Response response;
+
+    @Before
+    public void setup() {
+        userClient = new UserClient();
+    }
+
+    @Test
+    @DisplayName("Create unique user")
+    public void testUserIsCreated() {
+
+        user = User.getRandomUser();
+        response = userClient.create(user);
+        response.then().assertThat().body("success", equalTo(true)).and().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Creating a user that already exists")
+    public void testUserIsAlreadyRegister() {
+
+        user = User.getRandomUser();
+        userClient.create(user);
+        response = userClient.create(user);
+        response.then().assertThat().body("success", equalTo(false)).and().statusCode(403);
+    }
+
+    @Test
+    @DisplayName("Create user without filling field email")
+    public void testUserIsCreatedWithoutEmail() {
+
+        user = User.getRandomUser();
+        user.setEmail(null);
+        userClient.create(user);
+        response = userClient.create(user);
+        response.then().assertThat().body("success", equalTo(false)).and().statusCode(403);
+    }
+
+    @Test
+    @DisplayName("Creating a user without the required password field")
+    public void testUserIsCreatedWithoutPassword() {
+
+        user = User.getRandomUser();
+        user.setPassword(null);
+        userClient.create(user);
+        response = userClient.create(user);
+        response.then().assertThat().body("success", equalTo(false)).and().statusCode(403);
+    }
+
+    @After
+    public void deleteUser() {
+        userClient.deleteUser();
+    }
+}
